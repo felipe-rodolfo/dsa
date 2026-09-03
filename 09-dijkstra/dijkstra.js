84d19b58e8ce8f23e1da01825337bef65305d7b0
Felipe Rodolfo <felipewebdeveloper@gmail.com>
2026-09-03 17:33:26 -0300

    feat: implement Dijkstra algorithm with min heap

--- 09-dijkstra/dijkstra.js
+++ 09-dijkstra/dijkstra.js
@@ -1,21 +1,21 @@
 class MinHeap {
 
     constructor() {
-        this.head = []
+        this.heap = []
     }
 
     insert(element) {
-        this.head.push(element);
+        this.heap.push(element);
 
-        let currentIndex = this.head.length - 1;
+        let currentIndex = this.heap.length - 1;
         while (currentIndex > 0) {
 
             let currentParent = Math.floor((currentIndex - 1) / 2);
 
-            if (this.head[currentIndex].distance < this.head[currentParent].distance){
-                let tmp = this.head[currentIndex];
-                this.head[currentIndex] = this.head[currentParent];
-                this.head[currentParent] = tmp;
+            if (this.heap[currentIndex].distance < this.heap[currentParent].distance){
+                let tmp = this.heap[currentIndex];
+                this.heap[currentIndex] = this.heap[currentParent];
+                this.heap[currentParent] = tmp;
                 currentIndex = currentParent
             } else {
                 break;
@@ -25,14 +25,30 @@ class MinHeap {
     }
 
     extractMin() {
-        let min = this.head[0];
-        let lastItem = this.head.pop();
+        let min = this.heap[0];
+        let lastItem = this.heap.pop();
 
-        if(this.head.length) {
-            this.head[0] = lastItem;
+        if(this.heap.length) {
+            this.heap[0] = lastItem;
         }
 
         let currentIndex = 0;
+
+        while(true) {
+            let smallerChildIndex = this.getSmallerChildIndex(currentIndex);
+            if (smallerChildIndex == null) {
+                break;
+            }
+
+            if (this.heap[currentIndex].distance > this.heap[smallerChildIndex].distance) {
+                let tmp = this.heap[currentIndex];
+                this.heap[currentIndex] = this.heap[smallerChildIndex];
+                this.heap[smallerChildIndex] = tmp;
+                currentIndex = smallerChildIndex;
+            } else {
+                break;
+            }
+        }
 
         return min;
     }
@@ -41,17 +57,78 @@ class MinHeap {
         let leftChild = 2 * currentIndex + 1;
         let rightChild = 2 * currentIndex + 2;
 
-        if (leftChild >= this.head.length) {
+        if (leftChild >= this.heap.length) {
             return null;
         }
 
-        if (rightChild >= this.head.length) {
+        if (rightChild >= this.heap.length) {
             return leftChild;
         }
 
-        if (this.head[leftChild].distance > this.head[rightChild].distance) {
+        if (this.heap[leftChild].distance > this.heap[rightChild].distance) {
             return rightChild;
         }
         return leftChild;
     }
+
+    isEmpty(){
+        return this.heap.length === 0;
+    }
 }
+
+const graph = new Map();
+graph.set("A", [ {vertex: "B", weight: 4}, { vertex: "C", weight: 2}]);
+graph.set("B", [ {vertex: "E", weight: 3}, { vertex: "D", weight: 1}, { vertex: "A", weight: 4}]);
+graph.set("C", [ {vertex: "A", weight: 2}, { vertex: "D", weight: 5}]);
+graph.set("D", [ {vertex: "C", weight: 5}, { vertex: "B", weight: 1}, { vertex: "F", weight: 2}]);
+graph.set("E", [ {vertex: "B", weight: 3}]);
+graph.set("F", [ {vertex: "D", weight: 2}]);
+
+function dijkstra(graph, source, destination) {
+    let distances = new Map();
+    let previous = new Map();
+    let visited = new Set();
+
+    for (const vertex of graph.keys()) {
+        if (vertex === source) {
+            distances.set(vertex, 0);
+        } else {
+            distances.set(vertex, Infinity);
+        }
+    }
+
+    for (const vertex of graph.keys()) {
+        previous.set(vertex, null);
+    }
+
+    let priorityQueue = new MinHeap();
+    priorityQueue.insert({vertex: source, distance: 0});
+
+    while (!priorityQueue.isEmpty()) {
+        let current = priorityQueue.extractMin();
+        if(visited.has(current.vertex)) continue;
+        visited.add(current.vertex);
+        if(current.vertex === destination) break;
+        for (const neighbor of graph.get(current.vertex)) {
+            if (visited.has(neighbor.vertex)) continue;
+
+            let newDistance = distances.get(current.vertex) + neighbor.weight;
+
+            if (newDistance < distances.get(neighbor.vertex)) {
+                distances.set(neighbor.vertex, newDistance); 
+                previous.set(neighbor.vertex, current.vertex);
+                priorityQueue.insert({vertex: neighbor.vertex, distance: newDistance})
+            }
+        }
+    }
+
+    let path = [];
+    let step = destination;
+    while (step !== null) {
+        path.unshift(step);
+        step = previous.get(step)
+    }
+    return { distance: distances.get(destination), path: path}
+}
+
+console.log(dijkstra(graph, "A","F"));
